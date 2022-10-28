46025f8f6d44d0240201b1db5a27cd55578cdaeb
Lance Hudson <lance@lancehudson.com>
2022-10-28 13:13:55 -0400

    Update to 6.1.14

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -1,12 +1,12 @@
 cask "openlens" do
   arch arm: "-arm64"
 
-  version "6.1.1"
+  version "6.1.14"
 
   if Hardware::CPU.intel?
-    sha256 "aaf80c18832327858b10c75f133662debe303715908e49c899df99470a7acdb1"
+    sha256 "840bbf00210b81b3290bd6999faf01a4cb09b27802d0aa5a30ccbbcd7735c6f6"
   else
-    sha256 "53f3f0ac70d2d24656c19eae78237a9696766afad023ef3319a29b7fe0800e8d"
+    sha256 "494b361504e75b1aca4ca30093da5229737f7c5a1edd6b3a023d311ae3e4ddd8"
   end
 
   url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
